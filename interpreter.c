b65d5b76342e52487e882d58fb1975e7fff40faf
Noah S <noahsedlik@berkeley.edu>
2022-12-20 01:52:59 -0800

    header file setup

---
+++ interpreter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "interpreter.h"
+
+int main(int argc, char* argv) {
+    printf("hello world!");
+    return 0;
+}
+
+/**
+ * Checks if EXPRESSION is a valid BF expression. A valid expression consists
+ * of <>+-. tokens, or a matching set of [] with proper enclosed tokens.
+ * RETURNS:
+ * 0 (false) if EXPRESSION is not a valid BF expression.
+ * 1 (true) otherwise.
+*/
+int is_valid_expression(char *expression) {
+    return 0;
+}
+
+/**
+ * Runs EXPRESSION through the interpreter and returns the result if printing.
+*/
+void calculate_expression(char *expression) {
+    return;
+}
--- interpreter.h
+++ interpreter.h
@@ -1,4 +1,7 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+int is_valid_expression(char *expression);
+char *calculate_expression(char *expression);
+
 #endif
